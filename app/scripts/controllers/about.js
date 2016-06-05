7a1fe8d7c8aecff3acc341dbad792f44f43d8986
johndeyrup <john.deyrup@gmail.com>
2016-06-05 12:03:01 -0400

    added angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name housingProSeApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the housingProSeApp
+ */
+angular.module('housingProSeApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name housingProSeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the housingProSeApp
+ */
+angular.module('housingProSeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
